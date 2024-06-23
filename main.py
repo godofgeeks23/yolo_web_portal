5e1a16f36785ff247c1c0f7681bd304321e6617c
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-06-23 22:17:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ api_key = "YOUR_ROBOFLOW_API_KEY"
 
 # create an inference pipeline object
 pipeline = InferencePipeline.init(
-    model_id="yolov8x-1280", # set the model id to a yolov8x model with in put size 1280
-    video_reference="https://storage.googleapis.com/com-roboflow-marketing/inference/people-walking.mp4", # set the video reference (source of video), it can be a link/path to a video file, an RTSP stream url, or an integer representing a device id (usually 0 for built in webcams)
-    on_prediction=render_boxes, # tell the pipeline object what to do with each set of inference by passing a function
-    api_key=api_key, # provide your roboflow api key for loading models from the roboflow api
+    model_id="yolov8x-1280",  # set the model id to a yolov8x model with in put size 1280
+    # set the video reference (source of video), it can be a link/path to a video file, an RTSP stream url, or an integer representing a device id (usually 0 for built in webcams)
+    video_reference="https://storage.googleapis.com/com-roboflow-marketing/inference/people-walking.mp4",
+    # tell the pipeline object what to do with each set of inference by passing a function
+    on_prediction=render_boxes,
+    api_key=api_key,  # provide your roboflow api key for loading models from the roboflow api
 )
 
 # start the pipeline
